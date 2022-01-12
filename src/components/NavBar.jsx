8331f173e84dbb1efbb23b3666562fc6532d56f5
valentin-ruggieri <valeru.251@gmail.com>
2022-01-11 22:48:41 -0300

    NavBar en react

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function NavBar (){
+
+     return (  
+
+
+        <>
+<nav>
+
+
+<h1> Tienda De Ropa Basic </h1>
+
+
+<ul>  
+    
+    <li><a >Home</a></li>
+    <li><a >Novedades</a></li>
+    <li><a >Articulos</a>
+        <ul >
+            
+            <li><a > Remeras</a></li>
+            <li><a > Shorts </a></li>
+            <li><a > Buzos </a></li>
+            <li><a > Chombas </a></li>
+            
+        </ul>   
+    </li>
+    <li><a>INGRESAR</a></li>
+
+</ul>
+
+
+
+</nav>
+        
+        
+        </>
+     )
+
+
+
+}
